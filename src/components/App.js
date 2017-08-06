3260ed16fee3782db3f59cf6fc298ec089e45188
Brian Han <hellobrianhan@gmail.com>
2017-08-06 18:17:29 -0500

    App and Icon components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,80 @@
 import React, { Component } from 'react';
+import Icon from './Icon';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: false });
+  }
   render() {
-    return <h1>Hello World!!!</h1>;
+    return this.state.isLoading
+      ? <div>Loading...</div>
+      : <div>
+          <section className="section section--intro">
+            <h1 className="heading">Brian Han</h1>
+            <p className="body-text">
+              Front-end developer at{' '}
+              <a className="link" href="https://console.bluemix.net/">
+                IBM Bluemix
+              </a>{' '}
+              and{' '}
+              <a className="link" href="http://carbondesignsystem.com/">
+                Carbon Design System
+              </a>.
+            </p>
+            <p className="body-text">Based in Austin, TX.</p>
+          </section>
+
+          <section className="section section--contact">
+            <h2 className="heading">Contact</h2>
+            <p className="body-text">thisisbrianhan@gmail.com</p>
+            <div className="icon-container">
+              <a href="https://github.com/hellobrian">
+                <Icon
+                  className="icon icon--github"
+                  iconName="github"
+                  title="github"
+                  description="link to github profile"
+                />
+              </a>
+              <a href="https://twitter.com/_brianhan">
+                <Icon
+                  className="icon icon--twitter"
+                  iconName="twitter"
+                  title="twitter"
+                  description="link to twitter profile"
+                />
+              </a>
+              <a href="https://medium.com/@brianhan">
+                <Icon
+                  className="icon icon--medium"
+                  iconName="medium"
+                  title="medium"
+                  description="link to medium profile"
+                />
+              </a>
+              <a href="https://www.instagram.com/brianhan/">
+                <Icon
+                  className="icon icon--instagram"
+                  iconName="instagram"
+                  title="instagram"
+                  description="link to instagram profile"
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/hellobrian/">
+                <Icon
+                  className="icon icon--linkedin"
+                  iconName="linkedin"
+                  title="linkedin"
+                  description="link to linkedin profile"
+                />
+              </a>
+            </div>
+          </section>
+        </div>;
   }
 }
 
