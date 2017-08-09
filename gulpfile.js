dd14b53c030f6af1f81aab7ebfac6072d20195b0
Brian Han <hellobrian@users.noreply.github.com>
2017-08-08 21:26:50 -0500

    Use create-react-app (#3)
    * convert to create-react-app
    
    * gulp tasks for moving build files to root

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,18 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
+const del = require('del');
 
-gulp.task('sass:build', () => {
-  return gulp
-    .src('src/scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        outputStyle: 'expanded',
-      }).on('error', sass.logError)
-    )
-    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/css'));
+gulp.task('clean', () => {
+  del([
+    'build',
+    'static',
+    'asset-manifest.json',
+    '*.ico',
+    '*.html',
+    'manifest.json',
+    'service-worker.js',
+  ]);
 });
 
-gulp.task('sass:watch', ['sass:build'], () => {
-  gulp.watch('src/**/*.scss', ['sass:build']);
-});
-
-gulp.task('serve', ['sass:build'], () => {
-  browserSync.init({
-    server: '.',
-    open: false,
-  });
-
-  gulp.watch('src/**/*.scss', ['sass:build']);
-  gulp.watch('*.html').on('change', browserSync.reload);
-});
-
-gulp.task('default', () => {
-  console.log('Use npm scripts');
+gulp.task('move', () => {
+  return gulp.src('build/**').pipe(gulp.dest('.'));
 });
