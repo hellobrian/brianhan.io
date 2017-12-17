537721a9a972014dc6a47adbe6d7ece9d59b2458
Brian Han <brianhan87usa@gmail.com>
2017-12-16 21:33:13 -0600

    font-smoothing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
-// import './index.css';
-// import './prism-okaidia.css';
+import './index.css';
 
 const Header = () => (
   <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,12 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <ul>
+    <ul style={{ margin: '2rem' }}>
       <li>
         <Link to="/page-2/">Go to page 2</Link>
+      </li>
+      <li>
+        <Link to="/page-3">Go to page 3</Link>
       </li>
       <li>
         <Link to="/blog/test">Test blog</Link>
